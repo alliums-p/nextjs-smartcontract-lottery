74c72e14e067f9ab0d3fbbe8411821868a9dfe65
alliums-p <angnimas42@gmail.com>
2022-08-07 17:10:21 +0545

    adding notification on lottery entry

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import { useMoralis, useWeb3Contract } from "react-moralis";
 import { abi, contractAddresses } from "../constants";
 import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
 
 export default function LotteryEntrance() {
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
-
     const chainId = parseInt(chainIdHex);
-
     const lotteryAddress =
         chainId in contractAddresses ? contractAddresses[chainId][0] : null;
-
     const [lotteryFee, setLotteryFee] = useState("0");
+    const dispatch = useNotification();
 
     const { runContractFunction: enterLottery } = useWeb3Contract({
         abi: abi,
@@ -38,6 +37,21 @@ export default function LotteryEntrance() {
         }
     }, [isWeb3Enabled]);
 
+    const handleSuccess = async function (tx) {
+        await tx.wait(1);
+        handleNewNotification(tx);
+    };
+
+    const handleNewNotification = function (tx) {
+        dispatch({
+            type: "info",
+            message: "Transation Complete!",
+            title: "Transaction Notification",
+            position: "topR",
+            icon: "bell",
+        });
+    };
+
     return (
         <div>
             Hi from lottery entrance!
@@ -45,7 +59,10 @@ export default function LotteryEntrance() {
                 <div>
                     <button
                         onClick={async function () {
-                            await enterLottery();
+                            await enterLottery({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            });
                         }}
                     >
                         Enter Lottery
